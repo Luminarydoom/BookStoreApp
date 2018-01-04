808a602d547e740cea5459e2f332694f900bb97c
Taishawn Williams <twill4317@withs.org>
2018-01-03 19:33:17 -0500

    initial commit

---
+++ app/src/main/java/nyc/c4q/bookstoreapp/RSSPullService.java
@@ -0,0 +1,26 @@
+//package nyc.c4q.bookstoreapp;
+//
+//import android.app.IntentService;
+//import android.content.Intent;
+//import android.support.annotation.Nullable;
+//
+///**
+// * Created by MarckemX on 12/18/17.
+// */
+//
+//public class RSSPullService extends IntentService{
+//
+//    /**
+//     * Creates an IntentService.  Invoked by your subclass's constructor.
+//     *
+//     * @param name Used to name the worker thread, important only for debugging.
+//     */
+//    public RSSPullService(String name) {
+//        super(name);
+//    }
+//
+//    @Override
+//    protected void onHandleIntent(@Nullable Intent intent) {
+//
+//    }
+//}
